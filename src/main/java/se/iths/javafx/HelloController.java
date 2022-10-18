9924e958b323be9f065932708c988fef7c6679b1
julia <julia.lereback@gmail.com>
2022-10-18 15:38:17 +0200

    Add first button and label

--- src/main/java/se/iths/javafx/HelloController.java
+++ src/main/java/se/iths/javafx/HelloController.java
@@ -3,30 +3,45 @@ package se.iths.javafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
 
 public class HelloController {
-    @FXML
-    private Label textField;
 
+    HelloModel helloModel = new HelloModel();
+
+    @FXML
     public Button button1;
+    public Label label1;
+
+    @FXML
+    public TextField textField1;
+
+    @FXML
+    protected void initialize() {
+        textField1.textProperty().bindBidirectional(helloModel.textProperty());
+        //label1.textProperty().bind(helloModel.textProperty());
+    }
+
+
+
+
 
     @FXML
     protected void onHelloButtonClick() {
         button1.setText("Orange!");
     }
-
     public void buttonClicked(MouseEvent mouseEvent) {
         if (mouseEvent.isControlDown()) {
-            textField.setBackground(Background.fill(Color.ORANGE));
-            textField.setText("I am Orange");
+            label1.setBackground(Background.fill(Color.ORANGE));
+            label1.setText("I am Orange");
             button1.setText("Invisible");
         }
         else {
-            textField.setBackground(Background.fill(Color.TRANSPARENT));
-            textField.setText("I am invisible");
+            label1.setBackground(Background.fill(Color.TRANSPARENT));
+            label1.setText("I am invisible");
             button1.setText("Orange");
         }
 
---
+++ src/main/java/se/iths/javafx/HelloModel.java
@@ -0,0 +1,25 @@
+package se.iths.javafx;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class HelloModel {
+
+    private StringProperty text;
+
+    public HelloModel() {
+        text = new SimpleStringProperty();
+    }
+
+    public String getText() {
+        return text.get();
+    }
+
+    public StringProperty textProperty() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text.set(text);
+    }
+}
