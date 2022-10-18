473ace244f747b3c8e76dc26c89b85bbec9ab423
julia <julia.lereback@gmail.com>
2022-10-18 15:09:57 +0200

    Add first button and label

--- src/main/java/se/iths/javafx/HelloController.java
+++ src/main/java/se/iths/javafx/HelloController.java
@@ -1,14 +1,34 @@
 package se.iths.javafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label textField;
+
+    public Button button1;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        button1.setText("Orange!");
+    }
+
+    public void buttonClicked(MouseEvent mouseEvent) {
+        if (mouseEvent.isControlDown()) {
+            textField.setBackground(Background.fill(Color.ORANGE));
+            textField.setText("I am Orange");
+            button1.setText("Invisible");
+        }
+        else {
+            textField.setBackground(Background.fill(Color.TRANSPARENT));
+            textField.setText("I am invisible");
+            button1.setText("Orange");
+        }
+
     }
 }
